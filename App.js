6a8859b8fdff511b454c46a2b7f373d1d7449bfa
amrutadeo-22 <96867598+amrutadeo-22@users.noreply.github.com>
2022-12-20 11:28:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          TERMS AND CONDITIONS
+        </p>   
+      </header>
+    <ul className='content'>The Certificate as Youth Delegate will be given only if he/she fulfills the necessary conditions.</ul>
+    <ul className='content'>Abhyuday, IIT Bombay shall send all the publicity materials to the respective Youth Delegate but will not 
+               be liable for the extra costs incurred by the Youth Delegate.
+    </ul>
+    <ul className='content'>
+    The Certificates and Incentives shall be provided only on the successful completion of the complete term.
+    </ul>
+    <ul className='content'>
+    Abhyuday, IIT Bombay can change the terms and conditions if deemed necessary.
+    </ul>
+    </div>  
+  );
+}
+export default App;
